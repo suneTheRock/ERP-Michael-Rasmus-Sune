7f110d48d69b351abe7943fa418e29c3667b39c3
Fuzzyshark-Watson <Watson0_0@hotmail.com>
2022-03-18 13:31:43 +0100

    C# Repo

---
+++ ERPOpgave/ERPOpgave/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPOpgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//Kunde
+//Person
+//navn
+//Efternavn
+//Kontakt oplysninger: < List >
+//(Email
+//Telefonnummer)
+//Adresse(datatype)
+//Vej String
+//Nr String
+
+//Kunde: Person
+//Kundenr
+//Historik
+
+//Medarbejder : Person
+//(Til HR)
+
+//Produkt
+//Salgspris
+//Varenr
+//Unit
+//Indkøbspris
+//Beskrivelse
+//Leverandør
+//Navn
+//Lagerantal
+//Lokation
+//Farve
+//Størrelse
+//Vægt
+
+//Salg
+//Sælger
+//Odrenummer
+//Dato
+//Kunde
+//Valuta
+//Kun til viderebygning: List<Ordrenummer>
+        }
+    }
+}
