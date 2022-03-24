132cbc22e334d74efc88e081fad3728c8cc44765
suneTheRock <98324555+suneTheRock@users.noreply.github.com>
2022-03-24 12:27:41 +0100

    p1 kravene er lavet i Database klassen

--- ERPOpgave/ERPOpgave/Data/Database.cs
+++ ERPOpgave/ERPOpgave/Data/Database.cs
@@ -6,12 +6,52 @@ using System.Threading.Tasks;
 
 namespace ERPOpgave.Data
 {
-    public partial class Database
-    {
+        public Product p;
+        public List<Product> productList = new List<Product>();
+
         public static Database Instance { get; private set; }
         static  Database() 
         {
             Instance = new Database();
+            
         }
-    }
+
+        public Product GetProductById(int id)
+        {
+            foreach (Product p in productList)
+            {
+                if (p.ProductId == id)
+                    return p;
+            }
+            return p;
+        }
+
+        public Product GetAllProducts()
+        {
+            foreach(Product p in productList)
+            {
+                Console.WriteLine(p);
+            }
+            return p;
+
+        }
+
+        public void AddProduct()
+        {
+            productList.Add(p);
+        }
+
+        public void UpdateProduct(Product p,int Id)
+        {
+            p.ProductId = Id;
+        }
+
+        public void DeleteProductById(int id)
+        {
+            if(p.ProductId == id )
+            {
+                productList.Remove(p);
+            }
+            
+        }
 }
