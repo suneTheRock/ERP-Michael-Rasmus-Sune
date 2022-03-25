c32c33ac5098c3fce2c432ccbba1fba57ee5cd59
suneTheRock <98324555+suneTheRock@users.noreply.github.com>
2022-03-25 10:14:38 +0100

    opdate ProductMenuList

--- ERPOpgave/ERPOpgave/GUI/ProductMenuList.cs
+++ ERPOpgave/ERPOpgave/GUI/ProductMenuList.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TECHCOOL.UI;
 
 namespace ERPOpgave.GUI
 {
     internal class ProductMenuList : Screen
     {
         public override string Title { get; set; } = "LINE Security A/S";
+
+        protected override void Draw()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
