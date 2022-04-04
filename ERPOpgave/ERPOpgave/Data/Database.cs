821cb2975dc58b9b71806afec8ef0e13f6637823
Fuzzyshark-Watson <Watson0_0@hotmail.com>
2022-04-04 13:31:56 +0200

    Database Updated with Sales Orders

--- ERPOpgave/ERPOpgave/Data/Database.cs
+++ ERPOpgave/ERPOpgave/Data/Database.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ERPOpgave.Products;
-
+using ERPOpgave.Order;
 
 namespace ERPOpgave.Data
 {
-    public class Database
+    partial class Database
     {
         public Product p;
         public List<Product> productList = new List<Product>();
+        public List<SalesOrder> orderList = new List<SalesOrder>();
+
 
         public static Database Instance { get; private set; }
         static Database()
@@ -30,35 +32,67 @@ namespace ERPOpgave.Data
             }
             return p;
         }
+        public Product GetOrderById(int id)
+        {
+            foreach (Product p in productList)
+            {
+                if (p.ProductId == id)
+                    return p;
+            }
+            return p;
+        }
 
-        public Product GetAllProducts()
+        public void GetAllProducts()
         {
             foreach (Product p in productList)
             {
                 Console.WriteLine(p);
             }
-            return p;
 
         }
-
-        public void AddProduct()
+        public void GetAllSalesOrder()
         {
-            productList.Add(p);
+            foreach (SalesOrder order in orderList)
+            {
+                Console.WriteLine(order);
+            }
         }
-
+        public void AddProduct(Product product)
+        {
+            productList.Add(product);
+        }
+        public void AddSalesOrder(SalesOrder order)
+        {
+            orderList.Add(order);
+        }
         public void UpdateProduct(Product p, int Id)
         {
             p.ProductId = Id;
         }
+        public void UpdateProduct(SalesOrder order, int Id)
+        {
+            order.ID = Id;
+        }
 
         public void DeleteProductById(int id)
         {
-            if (p.ProductId == id)
+            foreach (Product p in productList)
             {
-                productList.Remove(p);
-            }
-
+                if (p.ProductId == id)
+                {
+                    productList.Remove(p);
+                }
+            }         
         }
-
+        public void DeleteSalesOrderById(int id)
+        {
+            foreach (SalesOrder order in orderList)
+            {
+                if (order.ID == id)
+                {
+                    orderList.Remove(order);
+                }
+            }               
+        }
     }
 }
