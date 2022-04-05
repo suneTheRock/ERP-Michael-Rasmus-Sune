0b985ff8a127f4cb3468cc4806ccb24c1e7cb206
suneTheRock <98324555+suneTheRock@users.noreply.github.com>
2022-04-05 14:50:23 +0200

    SQL opsætning

--- ERPOpgave/ERPOpgave/Data/Database.cs
+++ ERPOpgave/ERPOpgave/Data/Database.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ERPOpgave.Models;
+using System.Data.SqlClient;
 
 namespace ERPOpgave.Data
 {
@@ -11,13 +12,25 @@ namespace ERPOpgave.Data
     {
         List<Customer> customers = new List<Customer>();
         public static Database Instance { get; private set; }
+        SqlConnection conn = null;
         static  Database() 
         {
             Instance = new Database();
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "docker.data.techcollege.dk";
+            builder.UserID = "H1PD021122_Gruppe2";
+            builder.Password = "H1PD021122_Gruppe2";
+            Instance.conn = new SqlConnection(builder.ConnectionString);
+            Instance.conn.Open();
+            
+
+            
         }
         //Getting Customer based on ID
         public Customer GetCustomerFromID(int i)
         {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT firstName FROM Customers JOIN";
             foreach(Customer customer in customers)
             {
                 if (i == customer.CustomerID) { return customer; }
